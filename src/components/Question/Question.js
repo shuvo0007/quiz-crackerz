7503814cd6ad03a4c72965e4698f1e85c3c0e179
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-10-12 11:26:02 +0600

    tostify complete

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,29 +1,64 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Question = ({ questionDiv }) => {
   const { options, question, correctAnswer } = questionDiv;
   function correctAnswerCheck(clickedOption) {
-    if(correctAnswer==clickedOption){
-      console.log("correct");
-    }else{
-      console.log("wrong");
+    toast.success("Your Answer is CORRECT", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+    if (correctAnswer === clickedOption) {
+    } else {
+      toast.error("Your Answer is WRONG!!!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
     }
   }
   return (
     <div className=" border-2 border-green-500 shadow-lg shadow-green-500/50 rounded-2xl bg-green-700/20 hover:bg-green-800">
-      <h2 className="text-4xl m-5 ">
-        {question.replace(/(<([^>]+)>)|&nbsp;/gi, " ")}
-      </h2>
-      <div className="flex flex-wrap justify-between drop-shadow-lg ">
-        {options.map((option) => (
-          <button
-            onClick={()=> correctAnswerCheck(option)}
-            className="shadow-lg shadow-blue-500/50 border-2 border-sky-700 w-64 p-2 rounded-xl m-5 text-2xl bg-blue-900/50 hover:bg-blue-700"
-          >
-            {option}
-          </button>
-        ))}
+      <div>
+        <h2 className="text-4xl m-5 ">
+          {question.replace(/(<([^>]+)>)|&nbsp;/gi, " ")}
+        </h2>
+        <div className="flex flex-wrap justify-between drop-shadow-lg ">
+          {options.map((option) => (
+            
+            <button
+              onClick={() => correctAnswerCheck(option)}
+              className="shadow-lg shadow-blue-500/50 border-2 border-sky-700 w-64 p-2 rounded-xl m-5 text-2xl bg-blue-900/50 hover:bg-blue-700"
+            >
+              {option}
+            </button>
+          ))}
+        </div>
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
